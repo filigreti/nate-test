514e23ceba36064faebe49cfbb6cccfacea72d53
filigreti <aolfiligre@gmail.com>
2020-04-16 22:28:45 +0100

    scroll top

--- src/views/AccountSettings.vue
+++ src/views/AccountSettings.vue
@@ -1,138 +1,86 @@
 <template>
-  <div class="account-settings">
+  <div id="ak" class="account-settings">
     <div class="side"></div>
     <div class="account-settings-container">
       <div class="job-container">
-        <h1 style="font-size:1.1rem" class="header-one  py-3">
-          Claims Details
-        </h1>
+        <h1 style="font-size:1.1rem" class="header-one py-3">Claims Details</h1>
 
         <div class="home-job-container">
           <div class="home-job-summary pb-4">
             <div class="insurance-information">
               <div class="insurance-header pt-3 pb-2">
-                <h1 class="header-one purp ">
-                  Insurance Information
-                </h1>
+                <h1 class="header-one purp">Insurance Information</h1>
               </div>
               <div class="insurance-grid">
-                <div
-                  class="d-flex my-3 justify-content-center align-items-center flex-column"
-                >
-                  <div class="header-two">
-                    Total Premium
-                  </div>
-                  <div class="header-big mt-1 size">
-                    ₦600,000
-                  </div>
+                <div class="d-flex my-3 justify-content-center align-items-center flex-column">
+                  <div class="header-two">Total Premium</div>
+                  <div class="header-big mt-1 size">₦600,000</div>
                 </div>
                 <div
                   class="second d-flex my-3 justify-content-center align-items-center flex-column"
                 >
-                  <div class="header-two">
-                    Claims Due
-                  </div>
-                  <div class="header-big mt-1 size">
-                    ₦250,000
-                  </div>
+                  <div class="header-two">Claims Due</div>
+                  <div class="header-big mt-1 size">₦250,000</div>
                 </div>
-                <div
-                  class="d-flex my-3 justify-content-center align-items-center flex-column"
-                >
-                  <div class="header-two">
-                    Insurance Type
-                  </div>
-                  <div class="header-big mt-1 size">
-                    Motor insurance
-                  </div>
+                <div class="d-flex my-3 justify-content-center align-items-center flex-column">
+                  <div class="header-two">Insurance Type</div>
+                  <div class="header-big mt-1 size">Motor insurance</div>
                 </div>
               </div>
             </div>
             <div class="insurance-information information">
               <div class="insurance-header pt-3 pb-2">
-                <h1 class="header-one purp ">
-                  Vehicle’s Information
-                </h1>
+                <h1 class="header-one purp">Vehicle’s Information</h1>
               </div>
               <div class="information-grid">
                 <div class="d-flex flex-column">
-                  <div class="header-two small">
-                    Vehicle’s Manufacturer
-                  </div>
-                  <div class="header-one">
-                    Toyota
-                  </div>
+                  <div class="header-two small">Vehicle’s Manufacturer</div>
+                  <div class="header-one">Toyota</div>
                 </div>
                 <div class="d-flex flex-column">
-                  <div class="header-two small">
-                    Engine Number
-                  </div>
-                  <div class="header-one">
-                    H22A M03737
-                  </div>
+                  <div class="header-two small">Engine Number</div>
+                  <div class="header-one">H22A M03737</div>
                 </div>
                 <div class="d-flex flex-column">
-                  <div class="header-two small">
-                    Vehicle’s Model
-                  </div>
-                  <div class="header-one">
-                    Toyota
-                  </div>
+                  <div class="header-two small">Vehicle’s Model</div>
+                  <div class="header-one">Toyota</div>
                 </div>
                 <div class="d-flex flex-column">
-                  <div class="header-two small">
-                    License Plate Number
-                  </div>
-                  <div class="header-one">
-                    ABC-123DE
-                  </div>
+                  <div class="header-two small">License Plate Number</div>
+                  <div class="header-one">ABC-123DE</div>
                 </div>
                 <div class="d-flex flex-column">
-                  <div class="header-two small">
-                    Chasis Number
-                  </div>
-                  <div class="header-one">
-                    5J6TF2H56AL006516
-                  </div>
+                  <div class="header-two small">Chasis Number</div>
+                  <div class="header-one">5J6TF2H56AL006516</div>
                 </div>
                 <div class="d-flex flex-column">
-                  <div class="header-two small">
-                    Vehicle Year
-                  </div>
-                  <div class="header-one">
-                    2019
-                  </div>
+                  <div class="header-two small">Vehicle Year</div>
+                  <div class="header-one">2019</div>
                 </div>
                 <div class="d-flex flex-column">
-                  <div class="header-two small">
-                    Vehicle’s Color
-                  </div>
-                  <div class="header-one">
-                    Matte Black
-                  </div>
+                  <div class="header-two small">Vehicle’s Color</div>
+                  <div class="header-one">Matte Black</div>
                 </div>
               </div>
             </div>
             <div class="insurance-information information">
               <div class="insurance-header pt-3 pb-2">
-                <h1 class="header-one purp ">
-                  Uploaded Documents
-                </h1>
+                <h1 class="header-one purp">Uploaded Documents</h1>
               </div>
               <div class="my-3 d-flex align-items-center">
-                <img src="@/assets/file.svg" alt="" />
+                <img src="@/assets/file.svg" alt />
                 <div class="ml-3 header-one">IMG_2018_12_90.png</div>
               </div>
               <div class="my-3 d-flex align-items-center">
-                <img src="@/assets/file.svg" alt="" />
+                <img src="@/assets/file.svg" alt />
                 <div class="ml-3 header-one">IMG_2018_12_90.png</div>
               </div>
               <div class="my-3 d-flex align-items-center">
-                <img src="@/assets/file.svg" alt="" />
+                <img src="@/assets/file.svg" alt />
                 <div class="ml-3 header-one">IMG_2018_12_90.png</div>
               </div>
               <div class="my-3 d-flex align-items-center">
-                <img src="@/assets/file.svg" alt="" />
+                <img src="@/assets/file.svg" alt />
                 <div class="ml-3 header-one">IMG_2018_12_90.png</div>
               </div>
             </div>
@@ -144,41 +92,23 @@
             </div>
             <div class="insurance-information information">
               <div class="insurance-header pt-3 pb-2">
-                <h1 class="header-one purp ">
-                  Customer’s Information
-                </h1>
+                <h1 class="header-one purp">Customer’s Information</h1>
               </div>
               <div class="d-flex my-3 flex-column">
-                <div class="header-two small">
-                  Customer’s Fullname
-                </div>
-                <div class="header-one sizer">
-                  Chiamaka Angélique Kidjo
-                </div>
+                <div class="header-two small">Customer’s Fullname</div>
+                <div class="header-one sizer">Chiamaka Angélique Kidjo</div>
               </div>
               <div class="d-flex my-3 flex-column">
-                <div class="header-two small">
-                  Telephone Number
-                </div>
-                <div class="header-one sizer">
-                  0801 0123 345
-                </div>
+                <div class="header-two small">Telephone Number</div>
+                <div class="header-one sizer">0801 0123 345</div>
               </div>
               <div class="d-flex my-3 flex-column">
-                <div class="header-two small">
-                  Email Address
-                </div>
-                <div class="header-one sizer">
-                  sample@email.com
-                </div>
+                <div class="header-two small">Email Address</div>
+                <div class="header-one sizer">sample@email.com</div>
               </div>
               <div class="d-flex my-3 flex-column mb-5">
-                <div class="header-two small">
-                  Address
-                </div>
-                <div class="header-one sizer">
-                  5A, Jakande Estate, Alagomeji - Yaba, Lagos.
-                </div>
+                <div class="header-two small">Address</div>
+                <div class="header-one sizer">5A, Jakande Estate, Alagomeji - Yaba, Lagos.</div>
               </div>
             </div>
           </div>
@@ -189,7 +119,11 @@
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {
+    window.scrollTo(0, 0);
+  }
+};
 </script>
 
 <style>
